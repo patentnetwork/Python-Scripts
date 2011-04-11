f879605895a824ba411a9ad8b5350643487317b9
amyzhaoyu <ayu@hbs.edu>
2011-04-11 11:47:39 -0400

    added GraphML.py

---
+++ GraphML.py
@@ -0,0 +1,20 @@
+import sys
+
+sys.path.append("/home/ron/PythonBase")
+import SQLite
+
+#file = "/media/data/edward/backup/v2/final_r7.sqlite3"
+file = "/home/ysun/disambig/newcode/invpatC.upper.Jan2011.sqlite3"
+
+s = SQLite.SQLite(file, 'invpat')
+
+for x in range(1975, 2010, 3):
+    print x
+    # Lower Bound results (currently marked as upr because we only have upr)
+    s.igraph(where='AppYearStr BETWEEN %d AND %d' % (x, x+2), vx="invnum_N").g.save('/home/ayu/DVN/pat_%d_upr.graphml' % x)
+    # Upper Bound results
+#   s.igraph(where='AppYearStr BETWEEN %d AND %d' % (x, x), vx="new_invnum_N").g.save('pat_%d_upr.graphml' % x)
+
+# Entire US graph for all history
+#s.igraph(where='Country="US"', vx="invnum_N").g.save('pat_US_low.graphml')
+#s.igraph(where='Country="US"', vx="new_invnum_N").g.save('pat_US_upr.graphml')
